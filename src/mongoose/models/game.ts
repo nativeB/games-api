c49dbc822d787b6a3274c4e31ff3debadcec0442
quincy <qhutchison@infonaligy.com>
2022-04-23 13:23:29 +0000

    add model for game

---
+++ src/mongoose/models/game.ts
@@ -0,0 +1,59 @@
+import * as mongoose from "mongoose"
+import { GameTypes } from "../../types"
+import constants from "../../utils/constants"
+
+export interface GameInterface {
+  userId: number,
+  game: string,
+  playTime: number,
+  genre: string,
+  platforms: GameTypes[],
+}
+
+interface GameModelInterface extends mongoose.Model<GameDocumentInterface> {
+}
+
+export interface GameDocumentInterface extends mongoose.Document {
+  userId: number,
+  game: string,
+  playTime: number,
+  genre: string,
+  platforms: GameTypes[],
+}
+
+const GameSchema = new mongoose.Schema({
+  userId: {
+    type: Number,
+    required: true,
+    unique:true
+  },
+  game: {
+    type: String,
+    required: true,
+    unique:true
+  },
+  playTime: {
+    type: Number,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  platforms: {
+    type: String,
+    required: true,
+    enum: constants.GAME_TYPES
+  }
+},
+{
+  timestamps: true
+})
+const Game = mongoose.model<GameDocumentInterface, GameModelInterface>("Game", GameSchema)
+
+
+export { Game }
+
+
+
+
